baa394fe75f9a66dd4667f65b249c1217c7b0c19
BT_1N3_10 <jamesleafgo@gmail.com>
2025-01-10 13:44:01 -0500

    JAMES IN PARIS

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("goodbye worldz");
-        System.out.println("james says goodbye worldz");
+        System.out.println("Steven says goodbye worldz");
     }
 }
