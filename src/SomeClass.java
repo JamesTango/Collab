7adefd4a69586716d6857c1b0628d7b1e1e761e4
BT_1N3_10 <jamesleafgo@gmail.com>
2025-01-10 13:50:20 -0500

    JAMES IN PARIS

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -1,6 +1,12 @@
+import javax.swing.plaf.SpinnerUI;
+
 public class SomeClass {
     private int num;
     public SomeClass(int num) {
         this.num = num;
+    }
+
+    public void partner2Method() {
+        System.out.println("yippee!");
     }
 }
